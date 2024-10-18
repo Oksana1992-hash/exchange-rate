7544fd4cb3e7931fe132a416222c8387ba587ac4
Докс <nox23061992@gmail.com>
2024-10-18 12:10:16 +0300

    Добавили выпадающий список для выбора валюты из которой мы находим курс

--- Course.py
+++ Course.py
@@ -5,20 +5,20 @@ import json
 
 
 def generation_list_rates():
-    list_currency = []
-    answer = requests.get('https://open.er-api.com/v6/latest/RUB')
+    answer = requests.get(f'https://open.er-api.com/v6/latest/RUB')
     json_info = answer.json()
     list_currency = list(json_info['rates'].keys())
     return list_currency
 
 def func_exchange():
-    code = combo.get().upper()
-    if code:
-        answer = requests.get('https://open.er-api.com/v6/latest/RUB')
+    code_target = combo_to.get().upper()
+    code_base = combo_from.get()
+    if code_target and code_base:
+        answer = requests.get(f'https://open.er-api.com/v6/latest/{code_base}')
         json_info = answer.json()
-        if code in json_info['rates']:
-            rez =  json_info['rates'][code]
-            content_l.config(text=f'1RUB - {rez}{code}')
+        if code_target in json_info['rates']:
+            rez =  json_info['rates'][code_target]
+            content_l.config(text=f'1{code_base} - {rez}{code_target}')
             content_l.config(fg='green')
         else:
             content_l.config(text='Такого кода валюты не существует')
@@ -33,17 +33,22 @@ window.geometry('400x400')
 
 sp_curen = generation_list_rates()
 
-
-t_m = Label(text='Выберите код валюты:')
+t_m = Label(text='Выберите код исходной валюты:')
 t_m.pack(pady = [10, 10])
 
-combo = ttk.Combobox(values=sp_curen)
-combo.pack(pady = [10, 10])
+combo_from = ttk.Combobox(values=sp_curen)
+combo_from.pack(pady = [10, 10])
+
+t_m = Label(text='Выберите код конечной валюты:')
+t_m.pack(pady = [10, 10])
+
+combo_to = ttk.Combobox(values=sp_curen)
+combo_to.pack(pady = [10, 10])
 
 content_l = Label()
 content_l.pack(pady = [10, 10])
 
-btn = Button(text='Получить курс рубля', command=func_exchange)
+btn = Button(text='Конвертировать валюту', command=func_exchange)
 btn.pack(pady = [10, 10])
 
 
